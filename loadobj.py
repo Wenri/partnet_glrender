15b69207c3a7c28dbdcd2524853482eb7913d64d
Bingchen Gong <gongbingchen@gmail.com>
2019-10-31 17:01:18 +0800

    Convert to pyglet

--- loadobj.py
+++ loadobj.py
@@ -1,28 +1,17 @@
 import os
 import numpy as np
-from trimesh import load_mesh
+from pywavefront import Wavefront
 from showobj import ShowObj
 
-DATA_DIR = '/media/data/Research/partnet_export'
+#DATA_DIR = '/media/data/Research/partnet_export'
+DATA_DIR = '/Users/wenri/Research/partnet_export'
+
 
 def main(im_id):
     im_file = os.path.join(DATA_DIR, "{}.obj".format(im_id))
-    scene = load_mesh(im_file)
-
-    return
-    dim_min = [None, None, None]
-    dim_max = [None, None, None]
-    for name, v, f in obj_f_v.values():
-        for ax in range(len(dim_min)):
-            min_v = np.min(v[:, ax])
-            max_v = np.max(v[:, ax])
-            if not dim_min[ax] or min_v < dim_min[ax][0]:
-                dim_min[ax] = (min_v, name)
-            if not dim_max[ax] or max_v > dim_max[ax][0]:
-                dim_max[ax] = (max_v, name)
-    for min_obj, max_obj in zip(dim_min, dim_max):
-        print(min_obj, max_obj)
-    ShowObj(obj_f_v.values()).show_obj()
+    scene = Wavefront(im_file)
+    
+    ShowObj(scene).show_obj()
 
 
 if __name__ == '__main__':
--- showobj.py
+++ showobj.py
@@ -1,34 +1,31 @@
 import glfw
-from OpenGL.GL import *
-from OpenGL.GLU import *
-from OpenGL.GLUT import *
-from OpenGL.arrays.vbo import VBO
-from geometry_utils import *
+from pyglet.gl import *
+from pywavefront import visualization
 
 
 class ShowObj:
-    def __init__(self, obj_parts):
+    def __init__(self, scene):
         # Initialize the library
         if not glfw.init():
             raise Exception('An error occurred')
 
-        self.obj_buffers = [VertexBuffer(n, v, f-1) for n, v, f
-                            in obj_parts]
+        self.scene = scene
 
     def __del__(self):
-        del self.obj_buffers
         glfw.terminate()
 
     def perspective(self):
         glMatrixMode(GL_PROJECTION)
         glLoadIdentity()
-        x, y, width, height = glGetInteger(GL_VIEWPORT)
-        gluPerspective(45.0, width/height, 1, 10.0)
+        viewport = (GLint * 4)()
+        glGetIntegerv(GL_VIEWPORT, viewport)
+        x, y, width, height = viewport
+        gluPerspective(45.0, width / height, 1, 10.0)
 
     def viewpoint(self):
         glMatrixMode(GL_MODELVIEW)
         glLoadIdentity()
-        gluLookAt(0.0, 0.0, -4.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0)
+        gluLookAt(0.0, 2.0, 4.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0)
 
     def material(self):
         mat_specular = [1.0, 1.0, 1.0, 1.0]
@@ -41,10 +38,10 @@ class ShowObj:
 
     def lighting(self):
 
-        light_ambient = [0.2, 0.2, 0.2, 1.0]
-        light_diffuse = [1.0, 1.0, 1.0, 1.0]
-        light_specular = [1.0, 1.0, 1.0, 1.0]
-        light_position = [1.0, 1.0, -4.0, 0.0]
+        light_ambient = (GLfloat * 4)(0.2, 0.2, 0.2, 1.0)
+        light_diffuse = (GLfloat * 4)(1.0, 1.0, 1.0, 1.0)
+        light_specular = (GLfloat * 4)(1.0, 1.0, 1.0, 1.0)
+        light_position = (GLfloat * 4)(0.0, 4.0, 0.0, 0.0)
 
         glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient)
         glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse)
@@ -56,18 +53,15 @@ class ShowObj:
 
     def draw_model(self):
         self.lighting()
-        self.material()
+        # self.material()
 
-        glDepthFunc(GL_LESS)
-        glEnable(GL_DEPTH_TEST)
+        # glDepthFunc(GL_LESS)
+        # glEnable(GL_DEPTH_TEST)
 
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-        #glPolygonMode(GL_FRONT_AND_BACK, GL_FILL)
+        # glPolygonMode(GL_FRONT_AND_BACK, GL_FILL)
 
-        for b in self.obj_buffers:
-            print(b.name)
-            b.draw()
-            break
+        visualization.draw(self.scene, lighting_enabled=True)
 
         # glutSolidTeapot(1)
 
@@ -91,39 +85,4 @@ class ShowObj:
             glfw.swap_buffers(window)
 
             # Poll for and process events
-            glfw.poll_events()
-
-
-class VertexBuffer:
-    def __init__(self, name, v, f):
-        self.name = name
-
-        self.fn = np.stack([tri_normal(v[v1], v[v2], v[v3])
-                            for v1, v2, v3 in f])
-
-        def vec_normal(vid):
-            ind = np.any(f == vid, axis=1)
-            if not np.any(ind):
-                return np.array([0., 0., 0.], dtype=np.float32)
-            norm_vec = np.mean(self.fn[ind, :], axis=0)
-            norm = np.linalg.norm(norm_vec)
-            return norm_vec if norm == 0 else norm_vec / norm
-
-        self.vn = np.stack([vec_normal(i) for i in range(v.size)])
-        self.v, self.f = v,f
-        self.vbo = VBO(np.concatenate((v, self.vn)))
-        self.ibo = VBO(f, 'GL_STATIC_DRAW',
-                           'GL_ELEMENT_ARRAY_BUFFER')
-
-    def draw(self):
-        glEnableClientState(GL_VERTEX_ARRAY)
-        glEnableClientState(GL_NORMAL_ARRAY)
-        with self.vbo:
-            glVertexPointer(3, GL_FLOAT, GL_FALSE, None)
-            glNormalPointer(GL_FLOAT, GL_FALSE,
-                            c_void_p(self.v.nbytes))
-        with self.ibo:
-            glDrawElements(GL_TRIANGLES, self.ibo.data.size,
-                           GL_UNSIGNED_INT, None)
-        glDisableClientState(GL_NORMAL_ARRAY)
-        glDisableClientState(GL_VERTEX_ARRAY)
+            glfw.wait_events()
